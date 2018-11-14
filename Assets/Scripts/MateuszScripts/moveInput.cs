fa8d33466eb216e426838294d96f980a75aa18b1
MooseWarden <mateuszpiekut@mail.adelphi.edu>
2018-11-14 01:18:29 -0500

    finished rotation level

---
+++ Assets/Scripts/MateuszScripts/moveInput.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveInput : MonoBehaviour {
+
+    private Color og; //original color
+
+    private SpriteRenderer r;
+
+    public bool over;
+    public bool select;
+    public bool moving;
+
+    public float x;
+    public float y;
+
+    void Start () {
+        r = GetComponent<SpriteRenderer>();
+
+        og = r.color;
+
+        over = false;
+        select = false;
+        moving = false;
+
+        x = 0f;
+        y = 0f;
+	}
+	
+	void Update () {
+        transform.Translate(x * Time.deltaTime, y * Time.deltaTime, 0f, Space.World);
+    }
+
+    void OnMouseOver()
+    {
+        if (moving == false)
+        {
+            r.color = Color.cyan;
+            over = true;
+
+            if (Input.GetMouseButton(0) && over == true)
+            {
+                r.color = Color.yellow;
+                select = true;
+            }
+
+            if (Input.GetKey("right") && select == true)
+            {
+                x = 2.5f;
+                select = false;
+                moving = true;
+            }
+            if (Input.GetKey("left") && select == true)
+            {
+                x = -2.5f;
+                select = false;
+                moving = true;
+            }
+            if (Input.GetKey("up") && select == true)
+            {
+                y = 2.5f;
+                select = false;
+                moving = true;
+            }
+            if (Input.GetKey("down") && select == true)
+            {
+                y = -2.5f;
+                select = false;
+                moving = true;
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "dummy" || collision.gameObject.tag == "Battery" || collision.gameObject.tag == "wall")
+        {
+            x = 0f;
+            y = 0f;
+            moving = false;
+        }
+    }
+
+    void OnMouseExit()
+    {
+        r.color = og;
+        over = false;
+        select = false;
+    }
+}
---
+++ Assets/Scripts/MateuszScripts/rotation.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotation : MonoBehaviour {
+
+    public moveInput script;
+
+    public Transform battery;
+    public Transform panel;
+
+    Vector3 b;
+    Vector3 p;
+
+    Collider2D c;
+
+	// Use this for initialization
+	void Start () {
+        b = battery.position;
+        p = panel.position;
+
+        c = GetComponent<Collider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Battery")
+        {
+            b.x = p.x;
+            b.y = p.y;
+
+            battery.position = b;
+
+            if(battery.eulerAngles.z == 0)
+            {
+                battery.eulerAngles = new Vector3(0, 0, 90);
+            }
+            else if(battery.eulerAngles.z == 90)
+            {
+                battery.eulerAngles = new Vector3(0, 0, 0);
+            }
+
+
+            script.x = 0f;
+            script.y = 0f;
+            script.moving = false;
+
+            c.isTrigger = true;
+        }
+        else
+        {
+            collision.gameObject.GetComponent<moveInput>().x = 0f;
+            collision.gameObject.GetComponent<moveInput>().y = 0f;
+            collision.gameObject.GetComponent<moveInput>().moving = false;
+            c.isTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        c.isTrigger = false;
+    }
+}

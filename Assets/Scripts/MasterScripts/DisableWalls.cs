9d19dec95dba80eb5e0e4f19d3363d35a3c5b805
Rarrack <44354378+Rarrack@users.noreply.github.com>
2019-06-09 15:50:10 -0400

    Disable Walls
    Edited and finalized disable walls script

---
+++ Assets/Scripts/MasterScripts/DisableWalls.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableWalls : MonoBehaviour
+{
+    //Brandon's MoveWalls
+    //Activates designated walls disable functions
+    //disables walls to be moved over when activated
+
+    #region Wall Disable Variables/Containers
+
+    bool activate = false;
+    public GameObject battery;
+    public List<GameObject> walls;
+
+    #endregion
+
+    void Start()
+    {
+    }
+
+    public bool Activate
+    {
+        get
+        {
+            return activate;
+        }
+        set
+        {
+            activate = value;
+        }
+    }
+
+    void Update()
+    {
+        // Tells all walls to move if the List is not empty
+        if (activate == true && walls != null)
+        {
+            for (int i = 0; i < walls.Count; i++)
+            {
+                if (walls[i].GetComponent<WallDisable>().disabled == false)
+                {
+                    walls[i].GetComponent<WallDisable>().Disable();
+                }
+                else
+                {
+                    walls[i].GetComponent<WallDisable>().Enable();
+                }
+            }
+
+            activate = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject == battery)
+        {
+            activate = true;
+        }
+    }
+}
---
+++ Assets/Scripts/MasterScripts/WallDisable.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallDisable : MonoBehaviour
+{
+    public bool disabled;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(disabled == false)
+        {
+            Enable();
+        }
+        else
+        {
+            Disable();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Disable()
+    {
+        Color curCol = GetComponent<SpriteRenderer>().color;
+        GetComponent<SpriteRenderer>().color = new Color(curCol.r, curCol.g, curCol.b, 0.5f);
+        GetComponent<BoxCollider2D>().enabled = false;
+    }
+
+    public void Enable()
+    {
+        Color curCol = GetComponent<SpriteRenderer>().color;
+        GetComponent<SpriteRenderer>().color = new Color(curCol.r, curCol.g, curCol.b, 255);
+        GetComponent<BoxCollider2D>().enabled = true;
+    }
+}

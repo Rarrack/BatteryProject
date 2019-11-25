41310dbd119d2caef85cdc32a8f8b2f264dd7697
Rarrack <44354378+Rarrack@users.noreply.github.com>
2019-11-25 03:15:28 -0500

    Stage Selection Transition
    Stage Selection Transition implemented into game. Works on button input to transition currently

---
+++ Assets/Scripts/MasterScripts/StageSwitch.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageSwitch : MonoBehaviour
+{
+    public List<Transform> transforms;
+    public int currentStage = 0;
+    public float easing = 0.5f;
+
+    public void NextStage()
+    {
+        currentStage += 1;
+        Vector3 newSpot = transform.position;
+        newSpot -= new Vector3(transforms[currentStage].position.x, 0, 0);
+        StartCoroutine(SmoothTransition(transform.position, newSpot, easing));
+    }
+
+    public void LastStage()
+    {
+        currentStage -= 1;
+        Vector3 newSpot = transform.position;
+        newSpot -= new Vector3(transforms[currentStage].position.x, 0, 0);
+        StartCoroutine(SmoothTransition(transform.position, newSpot, easing));
+    }
+
+    IEnumerator SmoothTransition(Vector3 startPos, Vector3 endPos, float seconds)
+    {
+        float t = 0f;
+        while (t <= 1.0f)
+        {
+            t += Time.deltaTime / seconds;
+            transform.position = Vector3.Lerp(startPos, endPos, Mathf.SmoothStep(0f, 1f, t));
+            yield return null;
+        }
+    }
+
+}
